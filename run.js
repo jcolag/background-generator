7abbf8a2604f7b5723cf655adb45a7e19df696a0
John Colagioia <john@colagioia.net>
2022-11-03 06:37:53 -0400

    Fix typos

--- run.js
+++ run.js
@@ -60,11 +60,11 @@ window.onload = function() {
 
 function processFile(filename, bytes) {
   const decoder = new TextDecoder('utf-8');
-  const json = decoder.decode(jsonBytes);
+  const json = decoder.decode(bytes);
   const idxFilename = externalFiles.indexOf(filename);
 
   // Remove file from list.
-  externalFiles = externalFiles.splice(idxFilename, 1);
+  externalFiles.splice(idxFilename, 1);
 
   // Process the file based on its name.
 
